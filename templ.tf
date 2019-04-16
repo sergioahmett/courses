fabb70ff79b354e98a31706a8446651205133a08
Serhii Akhmetshyn <serhii_akhmetshyn@epam.com>
2019-04-16 13:06:54 +0300

    up

--- templ.tf
+++ templ.tf
@@ -1,11 +1,17 @@
 provider "aws" {
   region = "ap-northeast-1"
 }
+  
+resource "aws_instance" "test_VM" {
+  # Amazon Linux AMI 2017.03.1 (HVM)
+  ami           = "ami-449f483b"
+  instance_type = "t2.micro"
 
-resource "aws_key_pair" "deployer" {
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
-
-  provisioner "local-exec" {
-    command = "echo 'HAAAALOOOOO'"
+  tags {
+    Name = "${var.vm_name}"
   }
+}
+
+variable "vm_name" {
+  description = "Name for VM to be created1"
 }
